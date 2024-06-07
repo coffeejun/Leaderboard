1ecc644a405aeae50457fa521d312fab72e79bc7
austinli_i <austinli_i@didiglobal.com>
2024-06-07 16:25:05 +0800

    router放在单独的文件夹里 main只用来启动

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
+import "gorank/router"
 
 func main() {
 	//创建一个实例
-	r := gin.Default()
-	r.GET("/hello", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "hello world")
-	})
-	r.POST("/user/list", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "user list")
-	})
-	r.PUT("/user/add", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "user add")
-	})
-	r.DELETE("/user/delete", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "user delete")
-	})
+	r := router.Router()
 	r.Run(":9999")
 }
