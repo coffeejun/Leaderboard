04f94f673254f8a22bd39facd5740c363ee1fa5c
austinli_i <austinli_i@didiglobal.com>
2024-06-07 15:54:33 +0800

    gin-get

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	//创建一个实例
+	r := gin.Default()
+	r.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello world")
+	})
+	r.Run(":9999")
 }
