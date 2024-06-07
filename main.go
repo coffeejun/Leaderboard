04e3b44dd3e61191676ca5b8d0586a7d14a34ba2
austinli_i <austinli_i@didiglobal.com>
2024-06-07 15:29:58 +0800

    创建gin

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "111")
-	})
-	r.Run() // 在 0.0.0.0:8080 上启动服务
 
 }
